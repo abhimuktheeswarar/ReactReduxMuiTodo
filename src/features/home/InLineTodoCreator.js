729234e5c5a91c6fa04f3ec76fcedcdfd6c7991c
Abhi Muktheeswarar <abhi.muktheeswarar@redbus.com>
2020-05-10 11:24:28 +0530

    Replace Final-Form with Formik

--- src/features/home/InLineTodoCreator.js
+++ src/features/home/InLineTodoCreator.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Form } from "react-final-form";
+//import { Form } from "react-final-form";
+import { Formik, Form, Field } from "formik";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Paper } from "@material-ui/core";
-import { TextField } from "mui-rff";
+//import { TextField } from "mui-rff";
+import { TextField } from "formik-material-ui";
 import { addTodo } from "./homeSlice";
 
 const useStyles = makeStyles((theme) => ({
@@ -20,48 +22,49 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default () => {
-  const [formState, setFormState] = useState();
+  //const [formState, setFormState] = useState();
   const dispatch = useDispatch();
-  const onSubmit = async (values) => {
+  const onSubmit = async (values, { setSubmitting }) => {
     console.log(JSON.stringify(values, 0, 2));
     //setFormState(values);
     //console.log(formState);
     dispatch(addTodo(values));
+    setSubmitting(false);
   };
   const classes = useStyles();
 
   return (
     <Paper className={classes.root}>
-      <Form
+      <Formik
+        initialValues={{
+          title: "",
+        }}
         onSubmit={onSubmit}
-        render={({ handleSubmit, form, submitting, pristine, values }) => (
-          <form
-            className={classes.form}
-            onSubmit={async (event) => {
-              await handleSubmit(event);
-              form.reset();
-            }}
-          >
-            <TextField
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form className={classes.form}>
+            <Field
               className={classes.textField}
               fullWidth
+              component={TextField}
               required
               name="title"
               label="What you want to do?"
               type="text"
             />
-
             <Button
               variant="contained"
               color="secondary"
               type="submit"
+              disabled={isSubmitting}
+              onClick={submitForm}
               className={classes.button}
             >
               ADD
             </Button>
-          </form>
+          </Form>
         )}
-      />
+      </Formik>
     </Paper>
   );
 };
